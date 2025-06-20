10ae5f14b89b7dafd313676e1d714b6406273554
Kushal98765 <kushalkumawat001@gmail.com>
2025-06-20 17:50:14 +0530

    last changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
 
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:5000//api/auth/login", { email, password });
+      const response = await axios.post("http://localhost:5000/api/auth/login", { email, password });
       if(response.data.success) {
         login(response.data.user)
         console.log(response.data.user)
